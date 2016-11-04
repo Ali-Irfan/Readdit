edd5114fd2ee878a8f801ba69e35e46ec4d9be7c
ThisBeAli <aliirfan@Alis-MacBook-Pro.local>
2016-11-03 23:42:42 -0400

    Initial Commit

---
+++ Readdit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Readdit
+//
+//  Created by Ali Irfan on 2016-11-03.
+//  Copyright © 2016 Ali Irfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
